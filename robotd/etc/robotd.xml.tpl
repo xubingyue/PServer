58d3df60d1a826435a68ae61d913eefc9eb62513
randyliu <randyliu@foxmail.com>
2014-05-29 14:45:44 +0800

    增加配置文件读取

---
+++ robotd/etc/robotd.xml.tpl
@@ -0,0 +1,4 @@
+<robotd_config>
+	<input_tbuskey>123456</input_tbuskey>
+	<output_tbuskey>654321</output_tbuskey>
+</robotd_config>
--- robotd/source/robotd.cpp
+++ robotd/source/robotd.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-extern "C"
-{
-#include "common.h"
-}
+
+#include "tapp.h"
+#include "robotd_config_reader.h"
+#include "robotd_config_types.h"
+
+robotd_config_t g_config;
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", test());
+	tapp_load_config(&g_config, argc, argv, (tapp_xml_reader_t)tlibc_read_robotd_config);
+
     return 0;
 }
 
